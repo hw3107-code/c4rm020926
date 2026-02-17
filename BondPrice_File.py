1df8a5fb75acaa56846f6815719cfecf74085f39
hw3107-code <hw3107@columbia.edu>
2026-02-16 19:38:23 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -2,7 +2,7 @@
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
     if ppy == 1:
-        x = 2170604
+        x = 170604
     if ppy == 2:
-        x = 2171686
+        x = 171686
     return(x)
